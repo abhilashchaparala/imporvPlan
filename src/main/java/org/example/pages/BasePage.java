b61f1488daba85efebc5380f3286356e4616a586
Abhilash Chaparala (DHL Express GHO), external <abhilash.chaparala@dhl.com>
2024-09-13 16:59:12 +0530

    sinar issues

--- src/main/java/org/example/pages/BasePage.java
+++ src/main/java/org/example/pages/BasePage.java
@@ -5,12 +5,18 @@ import com.microsoft.playwright.*;
 public class BasePage {
     Page page;
 
-    public Page initialise() {
+    public Page initializw() {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
                 .setHeadless(false));
         System.out.println("hello new quality issue");
         System.out.println("hello new quality issue");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
         }
